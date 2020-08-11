5a83ad4d8c8407825c49420b66f997fc242cc83c
Swadhikar C <swadhikar.chandramohan.ext@nokia.com>
2020-08-11 23:25:59 +0530

    html table

--- html_table/table.py
+++ html_table/table.py
@@ -1,51 +1,39 @@
 import pandas as pd
 
 data = {
-    'Name': ['Ticket', 'Raised', 'Severity', 'Cause', 'Status'],
-    'TeamA': [1, '12-Aug-2020 01:00:00', 'major', 'server unreachable', 'closed '],
-    'TeamB': [2, '13-Aug-2020 06:00:00', 'minor', '/tmp/ space filled', 'started'],
-    'TeamC': [3, '11-Aug-2020 03:00:00', 'major', 'server unreachable', 'closed '],
-    'TeamD': [4, '10-Aug-2020 02:00:00', 'minor', '/tmp/ space filled', 'started'],
-    'TeamE': [5, '11-Aug-2020 05:00:00', 'major', 'server unreachable', 'closed ']
+    'Title': ['Ticket', 'Team', 'Raised', 'Severity', 'Cause', 'Status'],
+    'ticket1': [1, 'TeamA', '12-Aug-2020 01:00:00', 'major', 'server unreachable', 'closed '],
+    'ticket2': [2, 'TeamB', '13-Aug-2020 06:00:00', 'minor', '/tmp/ space filled', 'started'],
+    'ticket3': [3, 'TeamC', '11-Aug-2020 03:00:00', 'major', 'server unreachable', 'closed '],
+    'ticket4': [4, 'TeamD', '10-Aug-2020 02:00:00', 'minor', '/tmp/ space filled', 'started'],
+    'ticket5': [5, 'TeamE', '11-Aug-2020 05:00:00', 'major', 'server unreachable', 'closed '],
+    'ticket6': [6, 'TeamF', '13-Aug-2020 06:00:00', 'minor', '/tmp/ space filled', 'started'],
+    'ticket7': [7, 'TeamG', '13-Aug-2020 06:00:00', 'minor', '/tmp/ space filled', 'started'],
+    'ticket8': [8, 'TeamH', '13-Aug-2020 06:00:00', 'minor', '/tmp/ space filled', 'started'],
+    'ticket9': [9, 'TeamI', '13-Aug-2020 06:00:00', 'minor', '/tmp/ space filled', 'started']
 }
 
 html_string = '''
 <html>
 <head>
-    <title>HTML Pandas Dataframe with CSS</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>
+        Service Now | Tickets
+    </title>
+    <link rel="stylesheet" href="styles.css">
 </head>
-<link rel="stylesheet" type="text/css" href="df_style.css"/>
 <body>
-    
-    {table}
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            crossorigin="anonymous"></script>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-            crossorigin="anonymous"></script>
-
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-            crossorigin="anonymous"></script>
-
-    <script type='text/javascript'>
-      window.onload = function() {{
-        const thead = document.querySelectorAll('.table > thead');
-        thead.forEach(e => e.classList.add('thead-light'));
-      }}
-    </script>
-
+<h1><span class="blue">&lt;</span>Table<span class="blue">&gt;</span> <span class="yellow">Responsive</span></h1>
+<h2>Tabular view of <a href="#" target="_blank">Service Now Tickets</a></h2>
+<div>
+{table}
+</div>
 </body>
 </html>
 '''
 
 df = pd.DataFrame(data)
-html_table = df.to_html(classes=["table", "table-dark"], index=False)
+df1 = df.set_index('Title').transpose()
+html_table = df1.to_html(classes=["container"], index=False)
 
 with open('table.html', 'w') as f:
     f.write(html_string.format(table=html_table))
